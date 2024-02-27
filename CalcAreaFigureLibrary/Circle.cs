aec1330ac5533c286a96f427fb5dd2bf3ebfa944
Alina Semidotchenko <96502823+illalinalli@users.noreply.github.com>
2024-02-27 22:16:36 +0400

    Добавьте файлы проекта.

---
+++ CalcAreaFigureLibrary/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcAreaFigureLibrary
+{
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            if (radius < 0) throw new InvalidOperationException("The radius of the circle is set incorrectly.");
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            if (radius < 0) throw new InvalidOperationException("The radius of the circle is set incorrectly.");
+            return Math.PI * radius * radius;
+        }
+    }
+}
